f31d1c386be0dec32cc177de273a4fb0256f1d34
lavdimimeri <45203738+lavdimimeri@users.noreply.github.com>
2021-04-22 17:31:28 +0200

    added some basic features
    Added only some basic stuff just to try it to deploy to the heroku clouds

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser=require('body-parser');
+
+const app= express();
+const port=process.env.PORT || 8888;
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+const users=[{name:'Tony', email:'tony@mail.com'}]
+
+app.get('/',(_,res)=>{
+    res.send('Your Express app');
+})
+app.get('/users',(_,res)=>{
+    res.json({ok:true,users});
+})
+
+app.get('/user/:name',(req,res)=>{
+ const{name}=req.params;
+ const user = users.filter((user)=>user.name===name)[0];
+ res.json({ok:true,user});
+})
+
+app.post('/adduser',(req,res)=>{
+    const {name,email}=req.body;
+    if(name&&email){
+        users.push({name,email});
+        res.json({ok:true,users});
+    }
+})
+
+app.listen(port,()=>{
+    console.log(`server is running on port: ${port}`);
+});
+
