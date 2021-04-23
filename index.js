26a6122148a97cefa4a74c0c4e9dc27ef98b1059
lavdimimeri <45203738+lavdimimeri@users.noreply.github.com>
2021-04-23 18:31:52 +0200

    First part of the lab2 is completed
    The part where we can add, substract, multiply and divide numbers by sending specific URLs

--- index.js
+++ index.js
@@ -30,6 +30,39 @@ app.post('/adduser',(req,res)=>{
     }
 })
 
+app.get('/calc',function(req,res){
+
+
+  //  console.log("Get customer with id");
+  //  console.log(req.query.id);
+  //  var customerID=req.query.id;
+  //  if(customerID==5){
+  //      res.send("Customer name: Lavdim Imeri")
+  //  }
+    
+    var operation=req.query.operation;
+    var numberone=parseInt(req.query.numberone);
+    var numbertwo=parseInt(req.query.numbertwo);
+    var data;
+
+    if(operation=="add"){
+        data=(numberone+numbertwo)
+        res.send("result is= "+ data)
+    }else if(operation=="sub"){
+      data=numberone-numbertwo;
+      res.send("result is= "+ data)
+    } else if(operation=="mul"){
+        data=numberone*numbertwo;
+      res.send("result is= "+ data)
+    }else if(operation=="div"){
+        data=numberone/numbertwo;
+      res.send("result is= "+ data)
+    } else
+       res.send("Unknown parameters") 
+
+
+})
+
 app.listen(port,()=>{
     console.log(`server is running on port: ${port}`);
 });
